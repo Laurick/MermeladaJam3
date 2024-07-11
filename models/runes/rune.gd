aadd6ea44e012999ccd3d24fef23a391045558c7
Laurick <javi.ethan@gmail.com>
2024-07-11 23:28:30 +0200

    mock rune and model

---
+++ models/runes/rune.gd
@@ -0,0 +1,6 @@
+class_name Rune
+extends Resource
+
+@export var name:String
+@export var description:String
+@export var image:Texture
---
+++ scripts/Global.gd
@@ -0,0 +1,6 @@
+extends Node
+
+#he/him = 0
+#she/her = 1
+#they/them = 2
+var gender: int = 0
