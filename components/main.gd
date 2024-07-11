bb08b6beefa5e78fe24c0a54222ea6f6ae4f3a54
LocrioPerdio <129734290+LocrioPerdio@users.noreply.github.com>
2024-07-11 22:18:10 +0200

    added main.gd

---
+++ components/main.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+var runas = []
+var piedras = []
+
+#aquí iría el is_equal que yo no sé codificar
+
+func _ready():
+	runas = ["Movimiento", "Seco", "Pelo", "Brillante", "Molestia" ,"Agua"]
+	piedras = ["Turquesa", "Obsidiana", "Cuarzo azul", "Citrino"]
+	
+	#incorporadas las runas y piedras incluidas en el guion a fecha 11/07- 22:12h. 
+	#Metemos mas, ademas de las que el guion refleja, para despistar al jugadore?
--- components/version_label.gd
+++
@@ -1,6 +0,0 @@
-class_name VersionLabel
-extends Label
-
-
-func _ready():
-	text = "v."+ProjectSettings.get_setting("application/config/version")
