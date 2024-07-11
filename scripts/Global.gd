96db3ca62355d88ceab0d73852d53957a53bb8bc
Laurick <javi.ethan@gmail.com>
2024-07-11 23:53:57 +0200

    update runes, added globals

--- scripts/Global.gd
+++ scripts/Global.gd
@@ -3,4 +3,11 @@ extends Node
 #he/him = 0
 #she/her = 1
 #they/them = 2
-var gender: int = 0
+var gender: int = -1
+var score: int = 0
+var affection_tomeu: int = 0
+
+func reset_game():
+	gender = -1
+	score = 0
+	affection_tomeu = 0
