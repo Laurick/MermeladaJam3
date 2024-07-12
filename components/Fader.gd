59ee4f0053eca37e6de49404b79c3e09f7109f11
Laurick <javi.ethan@gmail.com>
2024-07-12 02:40:27 +0200

    added colosus, stone and spells

---
+++ components/Fader.gd
@@ -0,0 +1,11 @@
+class_name Fader
+extends ColorRect
+
+
+func fade_in():
+	await get_tree().create_tween().tween_property(self,"color:a",1,2).finished
+	mouse_filter = Control.MOUSE_FILTER_STOP
+
+func fade_out():
+	await get_tree().create_tween().tween_property(self,"color:a",0,2).finished
+	mouse_filter = Control.MOUSE_FILTER_IGNORE
