8895e17a5e343cdb533d98838708104427edc61f
Laurick <javi.ethan@gmail.com>
2024-07-12 02:58:19 +0200

    some game loop thing

--- components/Fader.gd
+++ components/Fader.gd
@@ -1,11 +1,15 @@
 class_name Fader
 extends ColorRect
 
-
 func fade_in():
 	await get_tree().create_tween().tween_property(self,"color:a",1,2).finished
 	mouse_filter = Control.MOUSE_FILTER_STOP
 
 func fade_out():
+	await get_tree().create_tween().tween_property(self,"color:a",0,2).finished
+	mouse_filter = Control.MOUSE_FILTER_IGNORE
+
+func force_fade_out():
+	color.a = 1
 	await get_tree().create_tween().tween_property(self,"color:a",0,2).finished
 	mouse_filter = Control.MOUSE_FILTER_IGNORE
--- scenes/game.gd
+++ scenes/game.gd
@@ -1,4 +1,20 @@
 extends Control
 
-@onready var fader = $Fader
+@onready var fader:Fader = $Fader
 
+
+func _ready():
+	Global.reset_game()
+	DialogueManager.passed_title.connect(title_pased)
+	DialogueManager.dialogue_ended.connect(game_over.unbind(1))
+	fader.force_fade_out()
+	await get_tree().create_timer(2).timeout
+	DialogueManager.show_dialogue_balloon(load("res://dialogues/test.dialogue"))
+
+func title_pased(title:String):
+	print("passed: "+title)
+
+func game_over():
+	fader.fade_in()
+	await get_tree().create_timer(2).timeout
+	get_tree().change_scene_to_file("res://scenes/main.tscn")
